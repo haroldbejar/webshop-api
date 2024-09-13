b8beb6c78edbd57369679f4853bb626eee96a950
Harold <haroldbejarano@gmail.com>
2024-09-13 06:48:02 -0500

    Add .gitignore file

--- sanacommerce-webshoptests/obj/Debug/net8.0/sanacommerce-webshoptests.AssemblyInfo.cs
+++ sanacommerce-webshoptests/obj/Debug/net8.0/sanacommerce-webshoptests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sanacommerce-webshoptests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ebe97993dc1d9f683edaaacedd255ad151535f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84d9c70bc19e0944feacc9fbca57f99569a12348")]
 [assembly: System.Reflection.AssemblyProductAttribute("sanacommerce-webshoptests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sanacommerce-webshoptests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
