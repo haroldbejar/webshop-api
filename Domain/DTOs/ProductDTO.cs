5584b9b4d4ce28badc63c72ce31db9cb78723cbb
Harold <haroldbejarano@gmail.com>
2024-09-16 10:30:59 -0500

    Modifies productdtos - delelte tittle

--- Domain/DTOs/ProductDTO.cs
+++ Domain/DTOs/ProductDTO.cs
@@ -5,7 +5,6 @@
         public int ProductId { get; set; }
         public string ProductCode { get; set; }
         public string ProductName { get; set; }
-        public string ProductTitle { get; set; }
         public decimal Price { get; set; }
         public int Stock { get; set; }
         public string ImageUrl { get; set; }
